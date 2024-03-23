8a631fb90563449534cfea852e6320a649752c67
minhduc2307 <minhduc23072003@gmail.com>
2024-03-23 16:05:50 +0700

    Add section people & section question

---
+++ main.js
@@ -0,0 +1,18 @@
+// const $ = document.querySelector.bind(document);
+// const $$ = document.querySelectorAll.bind(document);
+
+// const dots = $$(".dots .dot");
+// const peopleItems = $$(".people-item");
+
+// const dotActive = $(".dot.active");
+
+// dots.forEach((dot, index) => {
+//     const people = peopleItems[index];
+
+//     dot.addEventListener("click", () => {
+//         $(".people-item.active").classList.remove("active");
+//         $(".dot.active").classList.remove("active");
+//         dot.classList.add("active");
+//         people.classList.add("active");
+//     });
+// });
